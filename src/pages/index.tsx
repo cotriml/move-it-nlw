5659f7213e0a68cf0e9857783ff5630c4b610a3e
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-02-24 21:09:01 -0300

    feat: add home component

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import { CompletedChallenges } from '../components/completed-challenges';
+import { Countdown } from '../components/countdown';
+import { ExperienceBar } from '../components/experience-bar';
+import { Profile } from '../components/profile';
+
+import Head from 'next/head'
+
+import styles from '../styles/pages/home.module.css'
+
+export default function Home () {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+
+
+      <ExperienceBar />
+
+      <section>
+        <div >
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+        </div>
+      </section>
+    </div>)
+}
